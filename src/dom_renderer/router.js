33849220d3a82bb36a4139c48ffed51f417972ac
Talpx1 <simo3401@gmail.com>
2023-05-21 15:39:20 +0200

    added favicon

--- src/dom_renderer/router.js
+++ src/dom_renderer/router.js
@@ -2,6 +2,7 @@ import { requirePage } from "./requirer";
 
 export default function router() {
     const url = new URL(window.location.href);
+
     let page = url.pathname.replace("/", "");
     
     if (page === undefined || page === null || page === '') page = 'home';
