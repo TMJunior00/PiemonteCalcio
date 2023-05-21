9afed79222695d4a2a858f4dc9fe1ec6980aca38
Talpx1 <simo3401@gmail.com>
2023-05-21 16:25:52 +0200

    added support for partial slots and children injection

--- src/dom_renderer/page_builder.js
+++ src/dom_renderer/page_builder.js
@@ -3,10 +3,21 @@ import { requirePartial } from "./requirer";
 async function insertPartials(body) {
     [...body.querySelectorAll("div span[data-partial]")].forEach(async placeholder => {
         const partial_name = placeholder.getAttribute("data-partial");
-        const partial_element = await requirePartial(partial_name);
-        placeholder.insertAdjacentElement("afterend", partial_element.body.childNodes[0]);
+        const partial_element = (await requirePartial(partial_name)).body.childNodes[0];
+        
+        insertPartialChildren(partial_element, placeholder)
+
+        placeholder.insertAdjacentElement("afterend", partial_element);
         placeholder.remove();
     }) 
+}
+
+function insertPartialChildren(partial_element, placeholder) {
+    const partial_slot = partial_element.querySelector("span[data-slot]");
+    if(partial_slot === null || partial_slot.length === 0) return;
+        
+    [...placeholder.children].forEach(e => partial_slot.insertAdjacentElement('afterend', e));
+    partial_slot.remove();
 }
 
 export default async function buildPageFromElement(element) {
